55fd30fa8143d11a5d25fc4146207c34a7d62a72
ADelCastillo1798 <adelcastillo1798@gmail.com>
2021-09-17 10:18:13 -0400

    part 1

--- part-1/app.py
+++ part-1/app.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+store = []
+
+@app.route('/')
+def fetch_jokes():
+    for _ in range(10):
+        r = requests.get('http://api.icndb.com/jokes/random/')
+        joke = r.json()['value']['joke']
+        store.append(joke)
+    
+@app.route('/getJokes')
+def return_jokes():
+    return jsonify(store)
+
+app.run()
